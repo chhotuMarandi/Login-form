1329624360f13a50ac6a39eee1ce37b6df9669d8
chhotu marandi <cmarandi29@gmail.com>
2022-05-13 21:10:38 +0530

    add js file

---
+++ script.js
@@ -0,0 +1,11 @@
+//selectors
+
+const emailInput = document.querySelector("email-input");
+const passInput = document.querySelector("pass-input");
+const loginBtn = document.querySelector("login-button");
+
+//Event Listeners
+
+loginBtn.addEventListener("click", function() {
+    console.log("hello");
+})
