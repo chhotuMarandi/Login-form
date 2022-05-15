58e0dcffe2c80bd98997823ba3ee9a93e85088d5
chhotu marandi <cmarandi29@gmail.com>
2022-05-15 10:42:46 +0530

    working in js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 //selectors
 
-const emailInput = document.getElementById("email-input");
-const passInput = document.getElementById("pass-input");
+const emailInput = document.querySelector(".email-input");
+const passInput = document.querySelector(".pass-input");
 const loginBtn = document.querySelector(".login-button");
 
 //Event Listeners
 
+loginBtn.addEventListener("click", function() {
+    event.preventDefault();
+    console.log("hello");
+})
 
+//Functions
+
+function checkForm() {
+    if(emailInput==="" && passInput==="") {
+        console.log("please fill the email and password");
+    }
+}
