9c8b374903dfbdccf1bcbc18978c41659eae07d2
chhotu marandi <cmarandi29@gmail.com>
2022-05-16 08:52:58 +0530

    add validation features whenever try to sumbit without filling from a error message should be appear

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const emailInput = document.querySelector(".email-input");
 const passInput = document.querySelector(".pass-input");
 const loginBtn = document.querySelector(".login-button");
+let errorElement = document.querySelector(".error"); 
 
 //Event Listeners
 
@@ -14,8 +15,14 @@ loginBtn.addEventListener("click", function() {
 //Functions
 
 function checkForm() {
+    let messages = [];
     if(emailInput.value =="" || passInput.value =="") {
-        alert("please fill the email and password");
+       alert("Please Fill Email & Password");
     }
+    if(emailInput.value.length < 8 || passInput.value.length < 8) {
+        alert("Password must be 8 Characters");
+        emailInput.value ="";
+        passInput.value ="";
+    }   
 }
 
